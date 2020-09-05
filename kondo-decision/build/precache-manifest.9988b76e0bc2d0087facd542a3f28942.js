fb9aa739cde641723d09590cc9e1afada6d4052b
howardmann <howardmann27@gmail.com>
2020-09-06 00:11:27 +1000

    surge

---
+++ kondo-decision/build/precache-manifest.9988b76e0bc2d0087facd542a3f28942.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52db5d4dd80576540e7ac0a15aff1e4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cce1fc0fbf60e232d993",
+    "url": "/static/css/main.d5e18cef.chunk.css"
+  },
+  {
+    "revision": "e503e156a6fe46ee0354",
+    "url": "/static/js/2.49d4b85b.chunk.js"
+  },
+  {
+    "revision": "806a98f318263a7af6eb94d73465b791",
+    "url": "/static/js/2.49d4b85b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce1fc0fbf60e232d993",
+    "url": "/static/js/main.f69e3361.chunk.js"
+  },
+  {
+    "revision": "3f5ee3ed4d473f6f612a",
+    "url": "/static/js/runtime-main.fa91e115.js"
+  }
+]);
